d4b97d0c7f72e8706e7c94dcb2b1946d9b166494
Lukáš Tyrychtr <ltyrycht@redhat.com>
2023-04-14 12:27:38 +0200

    Allow printing the list of visible apps and a tree for a particular app

--- print_accessibility_tree.py
+++ print_accessibility_tree.py
@@ -13,19 +13,31 @@ def print_object(object, level):
         print(f"Relations: {relation_descs}")
     if desc := object.get_description():
         print(f"Description: {desc}")
+    try:
+        box = object.queryComponent().getExtents(pyatspi.WINDOW_COORDS)
+        print(f"Object bounds: {box}")
+    except: pass
     for child in object:
         print_object(child, level + 1)
 
 @click.command
 @click.option("-f", "--focused", help="Print the tree for the next object which gains focus", is_flag=True)
-def main(focused):
+@click.option("-a", "--nth-app", help="Prints the tree for the nth application", default=None, type=int)
+@click.option("-l", "--list-apps", help="List the currently reachable applications", is_flag=True)
+def main(focused, nth_app, list_apps):
     if focused:
         print("Waiting for a focus event...")
         def handler(evt):
+            if not evt.detail1: return
             print_object(evt.source, 1)
             pyatspi.Registry.stop()
         pyatspi.Registry.registerEventListener(handler, "object:state-changed:focused")
         pyatspi.Registry.start()
+    elif list_apps:
+        for idx, app in enumerate(pyatspi.Registry.getDesktop(0)):
+            print(f"{idx + 1}: {app.name}")
+    elif nth_app is not None:
+        print_object(pyatspi.Registry.getDesktop(0)[nth_app - 1], 0)
     else:
         print_object(pyatspi.Registry.getDesktop(0), 0)
 
